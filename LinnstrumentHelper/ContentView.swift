7da14acca801652e723ebc30b704620d5108418b
MJR <mj.mcdougall@cymatica.art>
2020-04-20 22:04:13 -0400

    Initial Commit

---
+++ LinnstrumentHelper/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  LinnstrumentHelper
+//
+//  Created by Mark-James McDougall on 2020-04-20.
+//  Copyright © 2020 Mark-James M. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
